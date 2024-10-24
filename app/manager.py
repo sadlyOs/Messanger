560bea53be55972e66fa2fd27057b176e66e5197
sadlyOs <jukovkill@gmail.com>
2024-10-24 12:19:26 +0500

    test websocket

--- app/manager.py
+++ app/manager.py
@@ -2,26 +2,25 @@ import loguru
 from fastapi import WebSocket
 
 class ConnectManager:
-    def __init__(self, websocket: WebSocket) -> None:
-        self.active_connections = []
-        self.websocket = websocket
+    def __init__(self) -> None:
+        self.active_connections: list[WebSocket] = []
 
-    async def connect(self):
+    async def connect(self, websocket: WebSocket):
         """CONNECT EVENT"""
         await self.websocket.accept()
-        loguru.logger.info(self.websocket.keys())
-        self.active_connections.append(self.websocket)
+        loguru.logger.info(websocket.keys())
+        self.active_connections.append(websocket)
         loguru.logger.info(self.active_connections)
     async def send_personal_message(self, message: str):
         """DIRECT MESSAGE"""
-        await self.websocket.send_text(data=message)
+        await websocket.send_text(data=message)
         
     async def broadcast(self, message: str):
         loguru.logger.info(self.active_connections)
         for connection in self.active_connections:
             await connection.send_text(message)
 
-    def disconnect(self):
+    def disconnect(self, websocket: WebSocket):
         """disconnect event"""
-        self.active_connections.remove(self.websocket)
+        self.active_connections.remove(websocket)
 
